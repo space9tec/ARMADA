2f955f55a4199ead9b9c5c98a9c0a43733a14d8a
leulsm <leulsolm19@gmail.com>
2023-06-08 10:17:22 +0300

    change

--- armada/lib/view/screens/verify_account_screen/verify_serviceProvider_account_screen.dart
+++ armada/lib/view/screens/verify_account_screen/verify_serviceProvider_account_screen.dart
@@ -1,1 +1,21 @@
 import 'package:flutter/material.dart';
+
+class VerifyServiceProvider extends StatefulWidget {
+  const VerifyServiceProvider({super.key});
+
+  @override
+  State<VerifyServiceProvider> createState() => _VerifyServiceProviderState();
+}
+
+class _VerifyServiceProviderState extends State<VerifyServiceProvider> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
+  }
+}
